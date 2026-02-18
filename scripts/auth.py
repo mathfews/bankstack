7c0a63a724d22c02548a55c121be512c0d05c781
matheus <matheusfcw1010@gmail.com>
2026-02-18 19:53:42 -0300

    remove user_exits method and refreh function

--- scripts/auth.py
+++ scripts/auth.py
@@ -21,18 +21,16 @@ class Auth:
         with open(file_path, "w", encoding="utf-8") as arq:
             json.dump(self.database, arq, indent=4, ensure_ascii=False)
         return True, f"Usuário cadastrado, seja bem vindo {username}! | Seu current balance é ${self.database[username]["current balance"]}"
-
-    def withdraw(self, username, password):
-        if Auth.user_exists(username) == "False":
-            return False, "Usuário não encontrado"
-        self.database[username]["current balance"] += self.database[username]["pending incoming"]
-        with open(file_path, "r", encoding="utf-8") as arq:
-            json.dump(self.database, arq, indent=4, ensure_ascii=False)
-    def user_exists(self, username):
+    def withdraw(self, username):
         if username not in self.database:
-            return False
-        if username in self.database:
-            return True
+            return False, "* Usuário não encontrado"
+        if self.database[username]["pending incoming"] == 0:
+            return False, "* No pending incoming funds!"
+        self.database[username]["current balance"] += self.database[username]["pending incoming"]
+        self.database[username]["pending incoming"] = 0
+        with open(file_path, "w", encoding="utf-8") as arq:
+            json.dump(self.database, arq, indent=4, ensure_ascii=False)
+        return True, "* Withdrawal successful!"
     def login(self, username, password):
         if username not in self.database:
             return False, "Usuário não encontrado!"
@@ -40,20 +38,20 @@ class Auth:
             if cryptography(password) != self.database[username]["password"]:
                 return False, "Acesso negado!"
             return True, "Acesso concedido!"
-    def transfer(self,active_user,active_password, received,amount):
+    def transfer(self,active_user,active_password, recipient,amount):
         if active_user not in self.database:
             return False, "Usuário não encontrado!"
-        elif received not in self.database:
+        elif recipient not in self.database:
             return False, "O usuário que você deseja enviar não existe!"
-        if cryptography(active_password) != self.database[active_user]["password"]:
-            return False, "Incorrent password!"
+        if active_password != self.database[active_user]["password"]:
+            return False, "Incorret password!"
         if self.database[active_user]["current balance"] < amount:
             return False, "O usuário não tem saldo suficiente!"
         self.database[active_user]["current balance"] -= amount
-        self.database[received]["pending incoming"] += amount
+        self.database[recipient]["pending incoming"] += amount
         with open(file_path, "w", encoding="utf-8") as arq:
             json.dump(self.database, arq, indent=4, ensure_ascii=False)
-        return True, f"Transferência de ${amount} para {received} realizada com sucesso!\nAgora você tem ${self.database[active_user]["current balance"]}"
+        return True, f"Transferência de ${amount} para {recipient} realizada com sucesso!\nAgora você tem ${self.database[active_user]["current balance"]}"
     def show_balance(self, user, password):
         if user not in self.database:
             return False, "Esse usuário não existe!"
@@ -65,9 +63,12 @@ class Auth:
             return True, f"O usuário {user} tem o saldo ${self.database[user]["current balance"]}, mas ele tem ${self.database[user]["pending incoming"]}"
     def show_info(self, username):
         if username in self.database:
+            with open (file_path, "r", encoding="utf-8") as arq:
+                self.database = json.load(arq)
             info = {
                 "current balance": self.database[username]["current balance"],
-                "pending incoming": self.database[username]["pending incoming"]
+                "pending incoming": self.database[username]["pending incoming"],
+                "db_password": self.database[username]["password"]
             }
             return info
         else:
@@ -78,4 +79,5 @@ class Auth:
         self.database[user]["current balance"] += amount
         with open (file_path, "w", encoding="utf-8") as arq:
             json.dump(self.database, arq, indent=4, ensure_ascii=False)
-        return True, f"Deposito feito com sucesso! Agora o usuário você tem o saldo de ${self.database[user]["current balance"]}"
+        balance = self.database[user]["current balance"]
+        return True, f"Deposito feito com sucesso! Agora você tem o saldo de ${round(balance,2)}"
